2ce78e300a3a8150ad4a29f2033643da4c416023
Marios Avgeris <avgeris.marios@gmail.com>
2020-04-02 15:12:00 +0300

    randomize file selection for evaluation

--- src/main/java/Plotter.java
+++ src/main/java/Plotter.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.io.FileReader;
+import java.util.Random;
 
 import com.opencsv.CSVReader;
 
@@ -14,8 +16,53 @@ public class Plotter extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        File[] predFiles = new File(System.getProperty("user.dir") + "/evaluation_results/Prediction").listFiles();
+        File[] appFiles = new File(System.getProperty("user.dir") + "/evaluation_results/AppPerformance").listFiles();
+        File[] poiFiles = new File(System.getProperty("user.dir") + "/evaluation_results/PoiPerformance").listFiles();
+
+        // Pick 3 random files for the evaluation
+        Random rand = new Random();
+        int randomFileIndex;
+        File predFile, appFile, poiFile;
+        String predFilePath = "";
+        String appFilePath = "";
+        String poiFilePath = "";
+
+        randomFileIndex = rand.nextInt(predFiles.length);
+        for (File file : predFiles) {
+            if (!predFiles[randomFileIndex].isHidden() && predFiles[randomFileIndex].isFile()) {
+                predFilePath = predFiles[randomFileIndex].getPath();
+                System.out.println(predFilePath);
+                break;
+            }
+            else
+                randomFileIndex = rand.nextInt(predFiles.length);
+        }
+
+        randomFileIndex = rand.nextInt(appFiles.length);
+        for (File file : appFiles) {
+            if (!appFiles[randomFileIndex].isHidden() && appFiles[randomFileIndex].isFile()) {
+                appFilePath = appFiles[randomFileIndex].getPath();
+                System.out.println(appFilePath);
+                break;
+            }
+            else
+                randomFileIndex = rand.nextInt(predFiles.length);
+        }
+
+        randomFileIndex = rand.nextInt(poiFiles.length);
+        for (File file : poiFiles) {
+            if (!poiFiles[randomFileIndex].isHidden() && poiFiles[randomFileIndex].isFile()) {
+                poiFilePath = poiFiles[randomFileIndex].getPath();
+                System.out.println(poiFilePath);
+                break;
+            }
+            else
+                randomFileIndex = rand.nextInt(poiFiles.length);
+        }
+
         // Prediction evaluation
-        stage.setTitle("n-MMC Evaluation");
+        stage.setTitle("n-MMC Evaluation: " + predFilePath.substring(61));
         final NumberAxis yAxis = new NumberAxis(); // auto-ranging
         final CategoryAxis xAxis = new CategoryAxis();
 
@@ -30,7 +77,7 @@ public class Plotter extends Application {
         predicted.setName("Predicted");
         real.setName("Real");
 
-        try (CSVReader dataReader = new CSVReader(new FileReader("/Users/avgr_m/netmode-cloudsim/evaluation_results/Prediction/(Wed Apr 01 20:48:12 EEST 2020)_P5_A0.csv"))) {
+        try (CSVReader dataReader = new CSVReader(new FileReader(predFilePath))) {
             String[] nextLine;
             dataReader.skip(1); // Skip title
             while ((nextLine = dataReader.readNext()) != null) {
@@ -50,7 +97,7 @@ public class Plotter extends Application {
 
         // App Performance evaluation (1)
         Stage stage2 = new Stage();
-        stage2.setTitle("App Performance Evaluation (1/2)");
+        stage2.setTitle("App Performance Evaluation (1/2): " + appFilePath.substring(65));
         final NumberAxis yAxis2 = new NumberAxis(); // auto-ranging
         final CategoryAxis xAxis2 = new CategoryAxis();
 
@@ -65,7 +112,7 @@ public class Plotter extends Application {
         admitted.setName("Admitted");
         finished.setName("Finished");
 
-        try (CSVReader dataReader = new CSVReader(new FileReader("/Users/avgr_m/netmode-cloudsim/evaluation_results/AppPerformance/(Wed Apr 01 20:48:12 EEST 2020)_P5_A0.csv"))) {
+        try (CSVReader dataReader = new CSVReader(new FileReader(appFilePath))) {
             String[] nextLine;
             dataReader.skip(1); // Skip title
             while ((nextLine = dataReader.readNext()) != null) {
@@ -84,7 +131,7 @@ public class Plotter extends Application {
 
         // App Performance evaluation (2)
         Stage stage3 = new Stage();
-        stage3.setTitle("App Performance Evaluation (2/2)");
+        stage3.setTitle("App Performance Evaluation (2/2): " + appFilePath.substring(65));
         final NumberAxis yAxis3 = new NumberAxis(); // auto-ranging
         final CategoryAxis xAxis3 = new CategoryAxis();
 
@@ -99,7 +146,7 @@ public class Plotter extends Application {
         avgResponseTime.setName("Average");
         refResponseTime.setName("Reference");
 
-        try (CSVReader dataReader = new CSVReader(new FileReader("/Users/avgr_m/netmode-cloudsim/evaluation_results/AppPerformance/(Wed Apr 01 20:48:12 EEST 2020)_P5_A0.csv"))) {
+        try (CSVReader dataReader = new CSVReader(new FileReader(appFilePath))) {
             String[] nextLine;
             dataReader.skip(1); // Skip title
             while ((nextLine = dataReader.readNext()) != null) {
@@ -120,7 +167,7 @@ public class Plotter extends Application {
 
         // POI Performance evaluation (1)
         Stage stage4 = new Stage();
-        stage4.setTitle("POI Performance Evaluation (1/2)");
+        stage4.setTitle("POI Performance Evaluation (1/2): " + poiFilePath.substring(65));
         final NumberAxis yAxis4 = new NumberAxis(); // auto-ranging
         final CategoryAxis xAxis4 = new CategoryAxis();
 
@@ -133,7 +180,7 @@ public class Plotter extends Application {
 
         powerConsumption.setName("Power Consumption");
 
-        try (CSVReader dataReader = new CSVReader(new FileReader("/Users/avgr_m/netmode-cloudsim/evaluation_results/PoiPerformance/(Wed Apr 01 20:48:12 EEST 2020)_P5.csv"))) {
+        try (CSVReader dataReader = new CSVReader(new FileReader(poiFilePath))) {
             String[] nextLine;
             dataReader.skip(1); // Skip title
             while ((nextLine = dataReader.readNext()) != null) {
@@ -150,7 +197,7 @@ public class Plotter extends Application {
 
         // POI Performance evaluation (2)
         Stage stage5 = new Stage();
-        stage5.setTitle("POI Performance Evaluation (2/2)");
+        stage5.setTitle("POI Performance Evaluation (2/2): " + poiFilePath.substring(65));
         final NumberAxis yAxis5 = new NumberAxis(); // auto-ranging
         final CategoryAxis xAxis5 = new CategoryAxis();
 
@@ -163,7 +210,7 @@ public class Plotter extends Application {
 
         allocatedCores.setName("Allocated Cores");
 
-        try (CSVReader dataReader = new CSVReader(new FileReader("/Users/avgr_m/netmode-cloudsim/evaluation_results/PoiPerformance/(Wed Apr 01 20:48:12 EEST 2020)_P5.csv"))) {
+        try (CSVReader dataReader = new CSVReader(new FileReader(poiFilePath))) {
             String[] nextLine;
             dataReader.skip(1); // Skip title
             while ((nextLine = dataReader.readNext()) != null) {
